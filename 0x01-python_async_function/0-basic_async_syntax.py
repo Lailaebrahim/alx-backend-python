f434a0f0917b36afdeef7246f031325add580cef
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-07 21:35:09 +0300

    fix pep8 errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,21 +5,21 @@ random delay between 0 and max_delay (included and float value) seconds and
 eventually returns it.
 
 Args:
-  max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
+    max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
 
 Returns:
-  float: The random delay in seconds.
+    float: The random delay in seconds.
 """
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-  """
-  Asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10)
-  named wait_random that waits for a random delay between 0 and max_delay (included and float value)
-  seconds and eventually returns it.
-  """
-  delay: float = random.uniform(0, max_delay)
-  await asyncio.sleep(delay)
-  return delay
+    """
+    Asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10)
+    named wait_random that waits for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
+    """
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
