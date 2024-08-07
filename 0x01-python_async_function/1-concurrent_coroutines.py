a9bd2bc2b87186023c0fdf42711d3fba12592a58
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-07 22:11:34 +0300

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Asynchronous coroutine called wait_n that takes in 2
+int arguments (in this order): n and max_delay.
+You will spawn wait_random n times with the specified max_delay..
+
+Args:
+    max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
+
+Returns:
+    float: The random delay in seconds.
+"""
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Asynchronous coroutine called wait_n that takes in 2
+    int arguments (in this order): n and max_delay.
+    You will spawn wait_random n times with the specified max_delay.
+    """
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    delays = [ await wait_random(max_delay) for i in range(n)]
+    return delays
