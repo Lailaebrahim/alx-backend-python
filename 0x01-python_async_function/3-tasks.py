a5285f023e13fda04b25eeabc0891217dd90130b
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-07 22:54:44 +0300

    Task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""regular function that takes in an int argument
+and returns a asyncio.Task"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """regular function that takes in an int argument
+    and returns a asyncio.Task"""
+    return asyncio.create_tack(wait_random(max_delay))
