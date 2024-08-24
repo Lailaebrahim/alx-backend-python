4d6934870260141f7ff69d96a49645b94d6acaf3
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-25 01:56:10 +0300

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,17 +21,27 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mocked_get):
-        """_summary_
-
-        Args:
-            mocked_request (_type_): _description_
         """
+        Test get_json
+        
+        Args:
+            test_url (str): url
+            test_payload (dict): payload
+            mocked_get (Mock): mocked get
+        """
+        # create response mock object
         mocked_response = Mock()
+        # assign json return value to the response mock object
         mocked_response.json.return_value = test_payload
+        # assign the response mock object to the get method
         mocked_get.return_value = mocked_response
+        # call the get_json method with the test_url
         result = utils.get_json(test_url)
+        # assert the result is equal to the test_payload
         self.assertEqual(result, test_payload)
+        # assert the get method was called with the test_url
         mocked_get.assert_called_with(test_url)
+        # assert the get method was called once
         mocked_get.assert_called_once_with(test_url)
 
 
